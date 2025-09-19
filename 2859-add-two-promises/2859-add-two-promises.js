43080f776d07d43ce193c2f403cdd490e56c4766
Muhammed Fazal <fazalbkabeer@gmail.com>
2025-09-19 14:10:43 +0530

    Time: 53 ms (54.75%), Space: 54.8 MB (19.26%) - LeetHub

--- 2859-add-two-promises/2859-add-two-promises.js
+++ 2859-add-two-promises/2859-add-two-promises.js
@@ -4,8 +4,8 @@
  * @return {Promise}
  */
 var addTwoPromises = async function(promise1, promise2) {
-        let a =await promise1;
-        let b =await promise2;
+        const a =await promise1;
+        const b =await promise2;
     return a+b
 };
 
