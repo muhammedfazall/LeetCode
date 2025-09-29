7c221d97381cbe7aaf1928db87a7accc1e67b9f0
Muhammed Fazal <fazalbkabeer@gmail.com>
2025-09-29 11:59:53 +0530

    Time: 1 ms (58.02%), Space: 54.8 MB (49.6%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -3,13 +3,13 @@
  * @return {string}
  */
 var longestCommonPrefix = function (strs) {
-    if (strs.length === 0) return "";
-    const value = strs[0];
-    for (let i = 0; i < value.length; i++) {
+    if (!strs.length) return "";
+    const prefix = strs[0];
+    for (let i = 0; i < prefix.length; i++) {
         for (let j = 1; j < strs.length; j++) {
-            if (i >= strs[j].length || strs[j][i] !== value[i])
-                return value.slice(0, i);
+            if (i >= strs[j].length || strs[j][i] !== prefix[i])
+                return prefix.slice(0, i);
         }
     }
-    return value;
+    return prefix;
 };
