23a0bcaccde84ed9312168daa85825336fc7281b
Muhammed Fazal <fazalbkabeer@gmail.com>
2025-10-22 10:12:59 +0530

    Time: 42 ms (92.32%), Space: 55.9 MB (5.09%) - LeetHub

--- 2749-promise-time-limit/2749-promise-time-limit.js
+++ 2749-promise-time-limit/2749-promise-time-limit.js
@@ -3,17 +3,17 @@
  * @param {number} t
  * @return {Function}
  */
-var timeLimit = function(fn, t) {
-    
-    return async function(...args) {
+var timeLimit = function (fn, t) {
+
+    return async function (...args) {
         const p1 = fn(...args);
-        const p2 = new Promise((_, reject)=>{
-            setTimeout(()=>{
-            reject("Time Limit Exceeded")
-            },t)
+        const p2 = new Promise((_, reject) => {
+            setTimeout(() => {
+                reject("Time Limit Exceeded")
+            }, t)
         })
 
-        return Promise.race([p1,p2])
+        return Promise.race([p1, p2])
     }
 };
 
