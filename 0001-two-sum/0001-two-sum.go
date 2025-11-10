b7770c1ddeb17bca3ba7db294ad852fc2d7e4582
Muhammed Fazal <fazalbkabeer@gmail.com>
2025-11-10 16:44:49 +0530

    Time: 1 ms (55.33%), Space: 5.9 MB (39.8%) - LeetHub

--- 0001-two-sum/0001-two-sum.go
+++ 0001-two-sum/0001-two-sum.go
@@ -1,10 +1,12 @@
 func twoSum(nums []int, target int) []int {
-    for i:=0;i<len(nums)-1;i++{
-        for j:=i+1;j<len(nums);j++{
-            if nums[i] + nums[j] == target{
-            return []int{i,j}
-            }
+    m:= make(map[int]int)
+
+    for i,v := range nums{
+        diff := target - v;
+        if value,ok := m[diff];ok{
+            return []int{value,i}
         }
+        m[v] = i
     }
-    return []int{-1,-1}
+    return []int{}
 }
