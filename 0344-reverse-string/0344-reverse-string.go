5999baaa77f35eb91171943e5effcd5c52c996b8
Muhammed Fazal <fazalbkabeer@gmail.com>
2025-11-16 21:05:31 +0530

    Time: 0 ms (100%), Space: 8.4 MB (74.67%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.go
+++ 0344-reverse-string/0344-reverse-string.go
@@ -1,7 +1,8 @@
 func reverseString(s []byte)  {
-    j:=len(s)-1
-    for i:=0;i<len(s)/2;i++{
+    i,j:=0,len(s)-1
+    for i < j{
         s[i],s[j] = s[j],s[i]
+        i++
         j--
     }
 }
