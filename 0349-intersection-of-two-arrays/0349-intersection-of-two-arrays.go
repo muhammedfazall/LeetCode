3841d71430ece37fca5ae53f8e61e00fa3ae3660
Muhammed Fazal <fazalbkabeer@gmail.com>
2025-12-23 21:14:20 +0530

    Time: 0 ms (100%), Space: 4.7 MB (57.06%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.go
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.go
@@ -4,7 +4,6 @@ func intersection(nums1 []int, nums2 []int) []int {
 	for _,v := range nums1{
         m[v] = true
     }
-
     for _,v := range nums2{
         if m[v] {
             inter = append(inter,v)
