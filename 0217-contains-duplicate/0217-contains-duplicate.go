59ad6fc75b3b203f27cf44d14d6190dc7f1a9e06
Muhammed Fazal <fazalbkabeer@gmail.com>
2026-02-21 09:36:13 +0530

    Time: 14 ms (91.24%), Space: 14.6 MB (11.19%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.go
+++ 0217-contains-duplicate/0217-contains-duplicate.go
@@ -1,5 +1,10 @@
 func containsDuplicate(nums []int) bool {
-    freq := make(map[int]int)
+
+    if len(nums) == 1 {
+        return false
+    }
+
+    freq := make(map[int]int, len(nums))
 
     for _,v := range nums{
         freq[v]++
