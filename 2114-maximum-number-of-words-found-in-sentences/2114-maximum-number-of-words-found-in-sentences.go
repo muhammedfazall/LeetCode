2ace208147280799fd5b0cd56de8496a9f1acbf0
Muhammed Fazal <fazalbkabeer@gmail.com>
2026-02-28 11:39:48 +0530

    Time: 0 ms (100%), Space: 5.3 MB (86.39%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.go
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.go
@@ -1,20 +1,40 @@
 func mostWordsFound(sentences []string) int {
-	wordCount := map[string]int{}
-	for _,s := range sentences {
-		count := 0
-		for _,ch := range s {
-			if string(ch) == " " {
-				count++
-			}
-		}
-		wordCount[s] = count + 1
-	}
-
 	max := 0
-	for v := range wordCount {
-		if wordCount[v] > max {
-			max = wordCount[v]
-		}
-	}
-	return max
+
+    for _,s := range sentences {
+        count := 1
+
+        for i := 0; i < len(s) ; i++{
+            if s[i] == ' '{
+                count ++
+            }
+        }
+        if count > max {
+            max = count
+        }
+    }
+    return max
 }
+
+
+
+// func mostWordsFound(sentences []string) int {
+// 	wordCount := map[string]int{}
+// 	for _,s := range sentences {
+// 		count := 0
+// 		for _,ch := range s {
+// 			if string(ch) == " " {
+// 				count++
+// 			}
+// 		}
+// 		wordCount[s] = count + 1
+// 	}
+
+// 	max := 0
+// 	for v := range wordCount {
+// 		if wordCount[v] > max {
+// 			max = wordCount[v]
+// 		}
+// 	}
+// 	return max
+// }
