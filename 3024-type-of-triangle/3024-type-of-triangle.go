0f8a26c40f7da3cc94ff88d8281167b0fda74df6
Muhammed Fazal <fazalbkabeer@gmail.com>
2026-03-03 16:57:01 +0530

    Time: 0 ms (100%), Space: 4.1 MB (53.85%) - LeetHub

---
+++ 3024-type-of-triangle/3024-type-of-triangle.go
@@ -0,0 +1,14 @@
+func triangleType(nums []int) string {
+
+    if nums[0] + nums[1] <= nums[2] || nums[0] + nums[2] <= nums[1] || nums[1] + nums[2] <= nums[0]{
+        return "none"
+    }
+
+	if nums[0] == nums[1] && nums[0] == nums[2] {
+		return "equilateral"
+	} else if nums[0] == nums[1] || nums[0] == nums[2] || nums[1] == nums[2] {
+	return "isosceles"
+	} 
+
+    return "scalene"
+}
