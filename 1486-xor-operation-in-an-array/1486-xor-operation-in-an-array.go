71c4e9089f7a2e71041c2cdef2551af28a1c0f44
Muhammed Fazal <fazalbkabeer@gmail.com>
2026-04-04 10:30:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.go
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.go
@@ -1,9 +1,22 @@
 func xorOperation(n int, start int) int {
-    nums := make([]int,n)
     result := 0
     for i := 0 ; i < n ; i++ {
-        nums[i] = start + 2 * i
-        result ^= nums[i]
+        result ^= start + 2 * i
     }
     return result
 }
+
+
+
+//  // with slice
+//     result := 0
+//     for i := 0 ; i < n ; i++ {
+//         nums[i] = start + 2 * i
+//         result ^= nums[i]
+//     }
+//  // with two loop
+//     result := 0
+//     for _,v := range nums{
+//         result ^= v
+//     }
+//     return result
