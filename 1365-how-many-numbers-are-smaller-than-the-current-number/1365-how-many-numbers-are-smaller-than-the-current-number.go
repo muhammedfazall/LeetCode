6235c24c8db146e6cb248ce01e01b70b384def79
Muhammed Fazal <fazalbkabeer@gmail.com>
2026-04-04 10:58:07 +0530

    Time: 8 ms (13.43%), Space: 4.9 MB (53.43%) - LeetHub

---
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.go
@@ -0,0 +1,14 @@
+func smallerNumbersThanCurrent(nums []int) []int {
+    n := len(nums)
+    result := make([]int,n)
+    for i:= 0 ; i < n ; i ++ {
+        count := 0
+        for j:= 0 ; j < n ; j ++ {
+            if j != i && nums[j] < nums[i]{
+                count += 1
+            }
+        }
+        result[i] = count
+    }
+    return result
+}
