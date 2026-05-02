fd7a398664bfae8d14d15f6f5b13108b71c6eca2
Muhammed Fazal <fazalbkabeer@gmail.com>
2026-05-02 10:14:41 +0530

    Time: 0 ms (100%), Space: 4.9 MB (23.81%) - LeetHub

---
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.go
@@ -0,0 +1,12 @@
+func arraySign(nums []int) int {
+    sign := 1
+    for _,v := range nums{
+        if v == 0 {
+            return 0
+        }
+        if v < 0 {
+            sign = -sign
+        }
+    }
+    return sign
+}
