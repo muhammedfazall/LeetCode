119aeb58924d6f170e5be7cc4b7c8e1e52ec337f
Muhammed Fazal <fazalbkabeer@gmail.com>
2026-05-23 23:21:15 +0530

    Time: 0 ms (100%), Space: 4.6 MB (94.55%) - LeetHub

--- 2974-minimum-number-game/2974-minimum-number-game.go
+++ 2974-minimum-number-game/2974-minimum-number-game.go
@@ -1,21 +1,9 @@
 func numberGame(nums []int) []int {
-	n := len(nums)
-	for i := 0; i < n-1; i++ {
-		min := i
-		for j := i + 1; j < n; j++ {
-			if nums[j] < nums[min] {
-				min = j
-			}
-		}
-		if min != i {
-			nums[i], nums[min] = nums[min], nums[i]
-		}
-	}
+	
+    slices.Sort(nums)
 
-	for i := 0; i < n; i++ {
-		if i%2 != 0 {
-			nums[i], nums[i-1] = nums[i-1], nums[i]
-		}
+	for i := 0; i < len(nums); i+=2 {
+			nums[i], nums[i+1] = nums[i+1], nums[i]
 	}
 	return nums
 }
