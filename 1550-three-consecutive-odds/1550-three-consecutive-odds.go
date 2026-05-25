a539841571a5d4738702cc2b4e674d1234ca0f8b
Muhammed Fazal <fazalbkabeer@gmail.com>
2026-05-25 23:49:39 +0530

    Time: 0 ms (100%), Space: 4.5 MB (36.21%) - LeetHub

---
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.go
@@ -0,0 +1,15 @@
+func threeConsecutiveOdds(arr []int) bool {
+    count := 0
+
+    for _,v := range arr {
+        if v % 2 != 0{
+            count++
+            if count == 3{
+                return true
+            }
+        }else{
+            count = 0
+        }
+    }
+    return false
+}
