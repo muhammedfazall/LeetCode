2f9c616922ed0a4b51cbe3b0135172b0dc4d3300
Muhammed Fazal <fazalbkabeer@gmail.com>
2026-09-08 07:53:26 +0530

    Time: 0 ms (100%), Space: 5.4 MB (42.11%) - LeetHub

--- 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.go
+++ 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.go
@@ -1,13 +1,23 @@
 func prefixCount(words []string, pref string) int {
 	count := 0
-	n := len(pref)
-	for _, v := range words {
-		if len(v) >= n {
-			prefix := v[0:n]
-			if prefix == pref {
-				count++
-			}
-		}
+	for _, word := range words {
+		if strings.HasPrefix(word,pref){
+            count++
+        }
 	}
 	return count
 }
+
+// func prefixCount(words []string, pref string) int {
+// 	count := 0
+// 	n := len(pref)
+// 	for _, Word := range words {
+// 		if len(Word) >= n {
+// 			prefix := Word[0:n]
+// 			if prefix == pref {
+// 				count++
+// 			}
+// 		}
+// 	}
+// 	return count
+// }
