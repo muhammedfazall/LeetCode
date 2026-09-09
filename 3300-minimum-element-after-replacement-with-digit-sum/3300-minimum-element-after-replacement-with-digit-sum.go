598e351b4b2737db575040a3a866fb0e3a4c9d1b
Muhammed Fazal <fazalbkabeer@gmail.com>
2026-09-09 07:27:38 +0530

    Time: 1 ms (15.87%), Space: 4.7 MB (15.87%) - LeetHub

--- 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.go
+++ 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.go
@@ -1,12 +1,11 @@
 func minElement(nums []int) int {
-	min := nums[0]
-	for i, num := range nums {
+	min := math.MaxInt
+	for _, num := range nums {
 		sum := 0
 		for num > 0 {
 			sum += num % 10
 			num /= 10
 		}
-		nums[i] = sum
 		if sum < min {
 			min = sum
 		}
