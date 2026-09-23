2f7070f0773bede93474618dd1032588e0004dfd
Muhammed Fazal <fazalbkabeer@gmail.com>
2026-09-23 14:33:45 +0530

    Time: 4 ms (26.67%), Space: 5.4 MB (26.67%) - LeetHub

--- 2810-faulty-keyboard/2810-faulty-keyboard.go
+++ 2810-faulty-keyboard/2810-faulty-keyboard.go
@@ -1,25 +1,44 @@
+import (
+    "slices"
+)
+
 func finalString(s string) string {
 
-	str := []byte(s)
-	final := []byte{}
+	str := make([]rune,0,len(s))
 
-	for _, v := range str {
-		if v == 'i' {
-			reverse(final)
-		} else {
-			final = append(final, v)
-		}
+	for _, ch := range s {
+		switch ch{
+            case 'i' : 
+                slices.Reverse(str)
+            default  : 
+            str = append(str,ch)
+        }
 	}
-	return string(final)
+	return string(str)
 }
 
-func reverse(s []byte) []byte {
-	l := 0
-	r := len(s) - 1
-	for l < r {
-		s[l], s[r] = s[r], s[l]
-		l++
-		r--
-	}
-	return s
-}
+// func finalString(s string) string {
+
+// 	str := []byte(s)
+// 	final := []byte{}
+
+// 	for _, v := range str {
+// 		if v == 'i' {
+// 			reverse(final)
+// 		} else {
+// 			final = append(final, v)
+// 		}
+// 	}
+// 	return string(final)
+// }
+
+// func reverse(s []byte) []byte {
+// 	l := 0
+// 	r := len(s) - 1
+// 	for l < r {
+// 		s[l], s[r] = s[r], s[l]
+// 		l++
+// 		r--
+// 	}
+// 	return s
+// }
